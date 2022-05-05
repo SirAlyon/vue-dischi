5bede4343ceb802c664703d0c73a716e2473584d
SirAlyon <alessiosada04@gmail.com>
2022-05-05 15:55:18 +0200

    Add structure and installed bootstrap and axios

---
+++ src/components/SiteHeaderComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+
+export default{
+    name: 'SiteHeader',
+}
+</script>
+
+
+<style scoped lang="scss">
+    header{
+        height: 100px;
+        background-color: rgba(30, 45, 59, 1);
+    }
+</style>
+
+
---
+++ src/components/SiteMainComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+
+export default {
+    name: 'SiteMain',
+}
+</script>
+
+
+<style scoped lang="scss">
+    main{
+        height: calc(100vh - 100px);
+        background-color: rgba(46, 58, 70, 1);
+    }
+</style>
