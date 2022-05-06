5affea4be5c5e18617f272c38b23e8c10ab0de8c
SirAlyon <alessiosada04@gmail.com>
2022-05-06 17:23:14 +0200

    add filterDiscs method

--- src/components/SelectDiscComponent.vue
+++ src/components/SelectDiscComponent.vue
@@ -3,11 +3,10 @@
         <option v-for="(disc, index) in discs" :key="index" :value="disc.genre">{{disc.genre}}</option>
     </select> -->
 
-  <select name="selectGenre" 
-  :value="searchText" 
+  <select class="mt-5" name="selectGenre" 
   v-on:input="$emit('input', $event.target.value)"
-  @change="$emit('selectSubmit', $event)">
-    <option value="0">Nessun Filtro</option>
+  @change="$emit('selectSubmit')">
+    <option value="">Nessun Filtro</option>
     <option value="rock">Rock</option>
     <option value="pop">Pop</option>
     <option value="jazz">Jazz</option>
--- src/components/SiteMainComponent.vue
+++ src/components/SiteMainComponent.vue
@@ -2,11 +2,15 @@
   <main>
     <div class="container">
       <div class="row" v-if="!loading">
-        <Disc :disc="disc" v-for="(disc, index) in discs" :key="index" />
+        <Disc
+          :disc="disc"
+          v-for="(disc, index) in filteredDiscs"
+          :key="index"
+        />
       </div>
       <Loader v-else />
       <!-- <SelectDisc :discs="discs"/> -->
-      <SelectDisc v-model="searchText" @selectSubmit="search"/>
+      <SelectDisc v-model="searchText" @selectSubmit="filterDiscs"/>
     </div>
   </main>
 </template>
@@ -17,20 +21,22 @@ import Loader from "@/components/LoaderComponent.vue";
 import Disc from "@/components/DiscComponent.vue";
 import SelectDisc from "@/components/SelectDiscComponent.vue";
 
-
 export default {
   name: "SiteMain",
   components: {
     Loader,
     Disc,
-    SelectDisc
+    SelectDisc,
+
   },
   data() {
     return {
       link: "https://flynn.boolean.careers/exercises/api/array/music",
       loading: true,
       discs: null,
-      searchText: ''
+      searchText: "",
+      filteredDiscs: null
+      
     };
   },
   methods: {
@@ -41,18 +47,43 @@ export default {
           console.log(response.data);
           this.discs = response.data.response;
           this.loading = false;
+          this.filteredDiscs = this.discs
           console.log(this.discs);
         })
         .catch((error) => {
           console.log(error);
         });
     },
-    search(){
-        console.log('Cercando per...' + this.searchText);
-    }
+    search() {
+      console.log("Cercando per..." + this.searchText);
+    },
+     filterDiscs() {
+      if (this.discs !== null) {
+        this.filteredDiscs = this.discs.filter((disc) => {
+          return disc.genre
+            .toLowerCase()
+            .includes(this.searchText.toLowerCase());
+        });
+      } else {
+          return this.discs
+      }
+    },
+  },
+  computed: {
+    /* filterDiscs() {
+      if (this.discs !== null) {
+        return this.discs.filter((disc) => {
+          return disc.genre
+            .toLowerCase()
+            .includes(this.searchText.toLowerCase());
+        });
+      } else {
+          return this.discs
+      }
+    }, */
   },
   mounted() {
-      this.getApi()
+    this.getApi();
   },
 };
 </script>
